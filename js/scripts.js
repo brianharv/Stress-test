28462fdc3bb5ddfab41c76adfb11e7ab311ae706
David Cole <davidtcole@yahoo.com>
2020-08-18 14:00:50 -0700

    add branching structure and other jquery functionality.
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-event.preventDefault();
-})
+  $("#stress-survey").submit(function(event) {
+    event.preventDefault();
+    let warningArray = [];
+    let copingArray = [];
+    let symptomsArray = [];
+
+    $("input:checkbox[name=warning-signs]:checked").each(function() {
+      const warningSigns = $(this).val();
+      warningArray.push(warningSigns);
+    });
+
+    $("input:checkbox[name=coping]:checked").each(function() {
+      const copingMechanisms = $(this).val();
+      copingArray.push(copingMechanisms);
+    });
+
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      const healthSymptoms = $(this).val();
+      symptomsArray.push(healthSymptoms);
+    });
+
+    if ((warningArray.length >= copingArray.length) && (warningArray.length > symptomsArray.length)) {
+      $("#warningsigns-output").show();
+    } else if ((symptomsArray.length >= copingArray.length) && (symptomsArray.length > warningArray.length)){
+      $("#healthsymptoms-output").show();
+    } else {
+      $("#coping-output").show();
+    }
+  });
+});
